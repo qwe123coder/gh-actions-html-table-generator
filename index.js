e4cc15a058ab8baef17dfe0ddaf4ce93dbd689a2
Anirudh  Anup <anirudh012009@gmail.com>
2021-08-24 10:59:34 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,16 +1,15 @@
-const core = require('@actions/core');
-const github = require('@actions/github');
-const fs = require('fs');
-const path = require('path');
-const readmeBox = require('readme-box').ReadmeBox;
-const chunk = require('chunk');
+const core = require("@actions/core");
+const github = require("@actions/github");
+const fs = require("fs");
+const path = require("path");
+const readmeBox = require("readme-box").ReadmeBox;
+const chunk = require("chunk");
 var myJsonAbc = require("jsonabc");
 var sorted = myJsonAbc.sortObj(path);
-const path = sorted
 
 const generateCell = (cell) => {
-    const objectFieldNames = JSON.parse(core.getInput('object-field-names'));
-    let htmlCell = core.getInput('html-cell');
+    const objectFieldNames = JSON.parse(core.getInput("object-field-names"));
+    let htmlCell = core.getInput("html-cell");
 
     objectFieldNames.forEach((name) => {
         htmlCell = htmlCell.replace(new RegExp(`{{ ${name} }}`), cell[name]);
@@ -18,35 +17,40 @@ const generateCell = (cell) => {
     console.log(objectFieldNames);
     console.log(htmlCell);
     return htmlCell;
-}
+};
 
 const generateRow = (columns, row) => {
     const cells = row.map((cell) => generateCell(cell));
 
     if (cells.length < columns) {
-        cells.push('<td></td>'.repeat(columns - cells.length));
+        cells.push("<td></td>".repeat(columns - cells.length));
     }
 
-    return `<tr>${cells.join('')}</tr>`;
+    return `<tr>${cells.join("")}</tr>`;
 };
 
 (async() => {
-    const githubToken = core.getInput('github-token');
-    const filePath = path.join(process.env.GITHUB_WORKSPACE, core.getInput('json-file-path'));
-    const columns = core.getInput('columns');
-    const data = fs.readFileSync(filePath, 'utf8');
+    const githubToken = core.getInput("github-token");
+    const filePath = sorted.join(
+        process.env.GITHUB_WORKSPACE,
+        core.getInput("json-file-path")
+    );
+    const columns = core.getInput("columns");
+    const data = fs.readFileSync(filePath, "utf8");
     const json = JSON.parse(data);
 
     try {
-        const content = chunk(json, columns).map((row) => generateRow(columns, row));
-        const table = `<table width="100%">${content.join('')}</table>`;
+        const content = chunk(json, columns).map((row) =>
+            generateRow(columns, row)
+        );
+        const table = `<table width="100%">${content.join("")}</table>`;
 
         await readmeBox.updateSection(table, {
-            owner: process.env.GITHUB_REPOSITORY.split('/')[0],
-            repo: process.env.GITHUB_REPOSITORY.split('/')[1],
-            branch: process.env.GITHUB_REF.split('/')[2],
+            owner: process.env.GITHUB_REPOSITORY.split("/")[0],
+            repo: process.env.GITHUB_REPOSITORY.split("/")[1],
+            branch: process.env.GITHUB_REF.split("/")[2],
             token: githubToken,
-            section: 'data-section',
+            section: "data-section",
         });
     } catch (error) {
         core.setFailed(JSON.stringify(error));
